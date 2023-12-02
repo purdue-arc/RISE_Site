0feb40e7df810ca9f05009019ed200bae453e670
pkurto16 <ptkurto3@gmail.com>
2023-12-01 22:38:40 -0500

    Started Refactor

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css'; // Link to your stylesheet
+import riseLogo from './assets/riselogo.png' ;
+function App() {
+    return (
+        <div className="landing-page">
+            <header className="landing-header">
+                <nav className="landing-nav">
+                    <ul>
+                        <a href="/" className="header-logo-link">
+                            <img src={riseLogo} alt="Logo" className="header-logo" />
+                        </a>
+                        <li className = "nav-link"><a href="about">ABOUT</a></li>
+                        <li className = "nav-link"><a href="registration">REGISTRATION</a></li>
+                        <li className = "nav-link"><a href="program">PROGRAM</a></li>
+                        <li className = "nav-link"><a href="sponsorship">SPONSORSHIP</a></li>
+                        <li className = "nav-link"><a href="contact-us">CONTACT US</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <main className="landing-main">
+                <section className="hero">
+                    <h1 className="hero-text">
+                        R. I. <span className="hero-number">2</span>. E.
+                    </h1>
+                    <p className="hero-date">March 30th, 2024</p>
+                    <p className="hero-location">Hosted at Purdue University</p>
+                    <button className="hero-button">REGISTER YOUR INTEREST</button>
+                </section>
+            </main>
+
+            {/* Add footer if needed */}
+        </div>
+    );
+}
+
+export default App;
