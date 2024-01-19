7251b55ab90290c8b3c715c04933cd98d49aaf18
pkurto16 <ptkurto3@gmail.com>
2024-01-19 16:43:33 -0500

    Program Page

--- src/components/program/Program.jsx
+++ src/components/program/Program.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import '../../Main.css';
 import riseLogo from '../assets/riselogo.png' ;
-import arcLogo from '../assets/arclogo.png' ;
-import purdueMessage from '../assets/purdue_message.png' ;
 import Footer from "../../Footer";
+import LocationDetails from "./location-details/LocationDetails";
 function Program() {
     return (
         <div className="landing-page">
@@ -25,32 +24,12 @@ function Program() {
             <main className="landing-main">
                 {/* Existing About section */}
                 <section className="hero">
-                    <h3 className="other-text">ABOUT</h3>
-                    <section className="padding-small"></section>
-                    <button className="hero-button"><a style={{color:"#ffffff", textDecoration: "none"}} href="registration">REGISTER YOUR INTEREST</a></button>
+                    <h3 className="other-text">PROGRAM</h3>
                 </section>
             </main>
             <section className="padding-large"></section>
 
-            {/* New AboutRISE section */}
-            <main className="landing-main">
-                <div className="bg-blue-100 p-10">
-                    <h1 className="text-4xl font-bold text-center mb-6">ABOUT RISE</h1>
-                    <div className="flex flex-col items-center gap-6">
-                        <img src="../assets/program1.png" alt="Description1" className="mb-6"/>
-                        <p className="text-lg mb-6">
-                            {/* Content */}
-                        </p>
-                        <img src="../assets/program1.png" alt="Description2" className="mb-6"/>
-                        <p className="text-lg mb-6">
-                            {/* Content */}
-                        </p>
-                        <button className="bg-blue-600 text-white font-bold py-2 px-4 rounded">INITIAL RISE ARTICLE
-                        </button>
-                    </div>
-                </div>
-            </main>
-            <section className="padding-large"></section>
+            <LocationDetails/>
             <Footer/>
         </div>
     );
---
+++ src/components/program/location-details/LocationDetails.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './LocationDetails.css'; // Make sure to create a corresponding CSS file
+import program1 from "../../assets/program1.png"
+
+const LocationDetails = () => {
+    return (
+        <div className="location-details">
+            <div className="location-image-container">
+                <img src={program1} alt="Stewart Center at Purdue University" />
+            </div>
+            <div className="location-content">
+                <h2 className="full-location"><div className="location">Location:</div><div className="location-text">Purdue University</div> </h2>
+                <p>Consistently ranked as one of the top engineering institutions in the country, Purdue University is dedicated to helping students succeed while developing research at the forefront of industry and discovery.</p>
+
+                <h2 className="full-location"><div className="location">Venue:</div><div className="location-text">The Stewart Center</div> </h2>
+                <p>The Stewart Center serves as a dedicated facility at the university for meetings, conferences, and activities.</p>
+            </div>
+            <p className="address">Address: 128 Memorial Mall Dr, West Lafayette, IN 47907</p>
+        </div>
+    );
+};
+
+export default LocationDetails;
