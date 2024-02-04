c90d2d7f9edcff789aaf0f978ea8570797798561
pkurto16 <ptkurto3@gmail.com>
2024-02-04 17:42:46 -0500

    Riseweek finished without grid lines

--- src/components/program/rise-week/RiseWeek.jsx
+++ src/components/program/rise-week/RiseWeek.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
-import '../../../Main.css';
-import program1 from "../../assets/program1.png";
+import './RiseWeek.css';
+
 function RiseWeek() {
     return (
-        <div className="location-details">
-            <div className="location-image-container">
-                <img src={program1} alt="Stewart Center at Purdue University" />
+        <div className="rise-week-container">
+            <div className="title">
+                RISE WEEK | 3/25 - 3/29
             </div>
-            <div className="location-content">
-                <h2 className="full-location"><div className="location">Location:</div><div className="location-text">Purdue University</div> </h2>
-                <p>Consistently ranked as one of the top engineering institutions in the country, Purdue University is dedicated to helping students succeed while developing research at the forefront of industry and discovery.</p>
-
-                <h2 className="full-location"><div className="location">Venue:</div><div className="location-text">The Stewart Center</div> </h2>
-                <p>The Stewart Center serves as a dedicated facility at the university for meetings, conferences, and activities.</p>
-                <p className="address">Address: 128 Memorial Mall Dr, West Lafayette, IN 47907</p>
+            <div className="week-grid">
+                <div className="day">
+                    <div className="day-name">Monday</div>
+                    <div className="day-event">Spotlight Day</div>
+                </div>
+                <div className="day">
+                    <div className="day-name">Tuesday</div>
+                    <div className="day-event">Spotlight Day</div>
+                </div>
+                <div className="day">
+                    <div className="day-name">Wednesday</div>
+                    <div className="day-event">Spotlight Day</div>
+                </div>
+                <div className="day">
+                    <div className="day-name">Thursday</div>
+                    <div className="day-event">Prelude Session</div>
+                </div>
+                <div className="day">
+                    <div className="day-name">Friday</div>
+                    <div className="day-event">Prelude Session</div>
+                </div>
             </div>
-
         </div>
     );
 }
