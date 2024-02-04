28351d6d5a01612b39838f14b11277b57c496d85
pkurto16 <ptkurto3@gmail.com>
2024-02-04 18:12:02 -0500

    Dollar amount colors

--- src/components/sponsorship/tier-list/TierList.jsx
+++ src/components/sponsorship/tier-list/TierList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './TierList.css';
 
-const Tier = ({ tierName, cost, benefits }) => {
+const Tier = ({ tierName, cost, benefits, tierColor }) => {
     return (
         <div className="tier">
             <div className="tier-name">{tierName}</div>
@@ -10,7 +10,7 @@ const Tier = ({ tierName, cost, benefits }) => {
                     <div key={index} className="benefit">{benefit}</div>
                 ))}
             </div>
-            <div className="cost">{cost}</div>
+            <div className={`cost ${tierColor}`}>{cost}</div>
         </div>
     );
 };
@@ -18,10 +18,10 @@ const Tier = ({ tierName, cost, benefits }) => {
 const TierList = () => {
     return (
         <div className="tier-list">
-            <Tier tierName="PLATINUM PARTNER" cost="$5,000+" benefits={["Dedicated Spotlight Day", "Special Recognition at RISE", "Dedicated Social Media Post"]} />
-            <Tier tierName="GOLD PARTNER" cost="$3,000+" benefits={["Prelude Session Slot", "Logo on Digital Promotion", "Logo on Merchandise"]} />
-            <Tier tierName="SILVER PARTNER" cost="$1,000+" benefits={["Logo on Website", "Additional Booth Space"]} />
-            <Tier tierName="RISE PARTICIPANT" cost="FREE" benefits={["Booth Space to Display Projects", "Application for Presentation Slot"]} />
+            <Tier tierName="PLATINUM PARTNER" cost="$5,000+" benefits={["Dedicated Spotlight Day", "Special Recognition at RISE", "Dedicated Social Media Post"]} tierColor="platinum" />
+            <Tier tierName="GOLD PARTNER" cost="$3,000+" benefits={["Prelude Session Slot", "Logo on Digital Promotion", "Logo on Merchandise"]} tierColor="gold" />
+            <Tier tierName="SILVER PARTNER" cost="$1,000+" benefits={["Logo on Website", "Additional Booth Space"]} tierColor="silver" />
+            <Tier tierName="RISE PARTICIPANT" cost="FREE" benefits={["Booth Space to Display Projects", "Application for Presentation Slot"]} tierColor="participant" />
         </div>
     );
 };
