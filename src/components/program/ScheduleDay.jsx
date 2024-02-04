d6622d74c9af0dd735caad2023f605f44f6d5ee3
Mert Karabulut <mkarabul@purdue.edu>
2024-02-04 18:02:16 -0500

    Add day schedule

--- src/components/program/ScheduleDay.jsx
+++ src/components/program/ScheduleDay.jsx
@@ -1,64 +1,31 @@
 import Event from "./Event";
 
 const events = [
-    { time: "9:00 AM", desc: "" },
-    { time: "9:30 AM", desc: "3/30/2023" },
-    { time: "10:00 AM", desc: "3/30/2023" },
-    { time: "10:30 AM", desc: "3/30/2023" },
-    { time: "11:00 AM", desc: "3/30/2023" },
-    { time: "11:30 AM", desc: "3/30/2023" },
-    { time: "12:00 PM", desc: "3/30/2023" },
-    { time: "12:30 PM", desc: "3/30/2023" },
-    { time: "1:00 PM", desc: "3/30/2023" },
-    { time: "1:30 PM", desc: "3/30/2023" },
-    { time: "2:00 PM", desc: "3/30/2023" },
-    { time: "2:30 PM", desc: "3/30/2023" },
+  { time: "9:00 AM", desc: "Event begins, Booths open" },
+  { time: "10:05 AM", desc: "Keynote Speaker/Opening Remarks" },
+  { time: "11:00 AM", desc: "Presentation" },
+  { time: "11:20 AM", desc: "Presentation" },
+  { time: "11:40 AM", desc: "Presentation" },
+  { time: "12:00 PM", desc: "Lunch begins" },
+  { time: "1:05 PM", desc: "Keynote Speaker" },
+  { time: "2:00 PM", desc: "Presentation/End of Lunch Period" },
+  { time: "2:20 PM", desc: "Presentation" },
+  { time: "2:40 PM", desc: "Presentation" },
+  { time: "3:00 PM", desc: "Presentation" },
+  { time: "3:20 PM", desc: "Presentation" },
+  { time: "3:40 PM", desc: "Presentation" },
+  { time: "4:05 PM", desc: "Keynote Speaker/Closing Remarks" },
+  { time: "5:00 PM", desc: "Event ends" },
 ];
 
-
-/*
-9:00 AM
-10:05 AM
-11:00 AM
-11:20 AM
-11:40 AM
-12:00 PM
-1:05 PM
-2:00 PM
-2:20 PM
-2:40 PM
-3:00 PM
-3:20 PM
-3:40 PM
-4:05 PM
-5:00 PM
-Event begins, Booths open
-Keynote Speaker/Opening Remarks
-Presentation
-Presentation
-Presentation
-Presentation/End of Lunch Period
-Presentation
-Presentation
-Presentation
-Presentation
-Presentation
-Keynote Speaker/Closing Remarks
-Event ends
-Keynote Speaker
-Lunch begins
-*/
 function ScheduleDay() {
-
-    return (
-        <div class="lightbluebox">
-            <h1>RISE | 3/30/2023 </h1>
-            <table>
-                {for(let i = 0; i < 15; i++) {
-
-                }}
-            </table>
-        </div>
-    );
+  return (
+    <div class="lightbluebox">
+      <h1>RISE | 3/30/2023 </h1>
+      {events.map(({ time, desc }) => (
+        <Event key={time} time={time} desc={desc} />
+      ))}
+    </div>
+  );
 }
 export default ScheduleDay;
