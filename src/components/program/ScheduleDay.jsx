b45b845c43ada430302e9e2d3144eaf01118dc4b
Mert Karabulut <mkarabul@purdue.edu>
2024-02-04 17:39:03 -0500

    prettier fix

--- src/components/program/ScheduleDay.jsx
+++ src/components/program/ScheduleDay.jsx
@@ -1,9 +1,8 @@
 function ScheduleDay() {
-    return (
-        <div class="lightbluebox">
-            <h1>RISE | 3/30/2023 </h1>
-            
-        </div>
-    );
+  return (
+    <div class="lightbluebox">
+      <h1>RISE | 3/30/2023 </h1>
+    </div>
+  );
 }
 export default ScheduleDay;
